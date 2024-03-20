f896133a1f6d33f7e4ac8e277a235b96c2d6fb12
Ankit Das <www.das5471@gmail.com>
2024-03-21 04:28:48 +0530

    Add responsive logo and handle logo click

--- components/shared/Navigation_Components/Logo.js
+++ components/shared/Navigation_Components/Logo.js
@@ -1,7 +1,49 @@
 import React from 'react';
-
+import { useMediaQuery, useTheme } from '@mui/material';
+import Image from 'next/image';
+import redditLogo from '@/public/assets/Reddit_Icon_FullColor.svg'
+import redditLogoWordmark from '@/public/assets/Reddit_Logo_Wordmark_OrangeRed.svg'
+import { useRouter } from 'next/navigation';
 const Logo = () => {
-    return <div>Logo</div>;
+    const theme = useTheme();
+    const isSmall = useMediaQuery(theme.breakpoints.down('lg'));
+    const router = useRouter();
+    const handleLogoClick = () => {
+        router.push('/');
+    }
+
+
+    return (
+        <div
+            onClick={handleLogoClick}
+            style={{ cursor: 'pointer' }}
+        >
+            {isSmall ? (
+                <Image
+                src={redditLogo}// replace with your logo path
+                alt="Logo"
+                width={35}
+                height={35}
+            />
+            ) : (
+                <>
+                <Image
+                    src={redditLogo}// replace with your logo path
+                    alt="Logo"
+                    width={35}
+                    height={35}
+                />
+                {` `}
+                <Image
+                    src={redditLogoWordmark}// replace with your logo path
+                    alt="Logo"
+                    width={100}
+                    height={35}
+                />
+                </>
+            )}
+        </div>
+    );
 };
 
 export default Logo;
